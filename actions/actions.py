b125aa8669de2b5ef25f95b70151107c4fc57670
Mohamed Aziz Bennour <MBennour@se.linedata.com>
2023-04-17 15:50:42 +0100

    Bor rojla

--- actions/actions.py
+++ actions/actions.py
@@ -1,49 +1,24 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
 
-
-# This is a simple example for a custom action which utters "Hello World!"
-
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
-from typing import Dict, Text, Any, List, Union
+from typing import Dict, Text, Any, List, Union,Optional
 from rasa_sdk import Action, Tracker
-from rasa_sdk.events import SlotSet, EventType
+from rasa_sdk.events import SlotSet, EventType,AllSlotsReset,ActionExecuted
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk import FormValidationAction
+from rasa_sdk.types import DomainDict
+from .verif_params.CheckParamsAction import CheckFilteredObj,CheckCreterias,CheckPage,CheckSecurity
 import json
-
-class ResetSlotsAction(Action):
-    def name(self) -> str:
-        return "action_reset_slots"
-
-    def run(self, dispatcher: CollectingDispatcher,
-        tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-    
-        return [SlotSet("stock_company", None), SlotSet("stock_number", None)]
-
+import random
 class BuyStockAction(Action):
 
     def name(self) -> Text:
         return "action_buy_stock"
+    
+    def dispatchMessage(self,dispatcher,intent,stock_number,stock_company,response_message):
+         
+        response_dict = {"intent": intent, "entities": [{"stock_number":stock_number},{"stock_company":stock_company}], "response": response_message}
+
+        dispatcher.utter_message(json.dumps(response_dict))
+        return []
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
@@ -54,32 +29,45 @@ class BuyStockAction(Action):
         
         # Get intent and extracted entities
         intent = latest_message['intent']['name']
-        stock_company = None
-        stock_number = None
-        
-        for entity in latest_message['entities']:
-            if entity['entity'] == 'stock_company':
-                stock_company = entity['value']
-            elif entity['entity'] == 'stock_number':
-                stock_number = entity['value']
-        
-        # Save extracted entities in slots
-        if stock_company:
-            tracker.slots['stock_company'] = stock_company
-        if stock_number:
-            tracker.slots['stock_number'] = stock_number
         stock_number = tracker.get_slot("stock_number")
         stock_company = tracker.get_slot("stock_company")
         
+        if stock_company:
+            event = ActionExecuted("action_check_security")
+            if not event:
+                response_message="Please enter a valid security name"
+                return[]
+            tracker.slots['stock_company'] = stock_company
+        if stock_number:
+            tracker.slots['stock_number'] = stock_number
+
+        if not stock_company and not stock_number:
+
+            response_message="Please provide the stocks number and the name of the security"
+            event=self.dispatchMessage(dispatcher,intent,stock_number,stock_company,response_message)
+            return []
+
+        elif not stock_company:
+            response_message="Please provide the name of the security"
+            event=self.dispatchMessage(dispatcher,intent,stock_number,stock_company,response_message)
+            return [SlotSet("stock_company", stock_company), SlotSet("stock_number", stock_number)]
+
+        elif not stock_number:
+
+            response_message="Please provide the number of stocks"
+            event=self.dispatchMessage(dispatcher,intent,stock_number,stock_company,response_message)
+            return [SlotSet("stock_company", stock_company), SlotSet("stock_number", stock_number)]
+        
         # Generate response message
         response_message = f"processing command..."
-        response_dict = {"intent": intent, "entities": [{"stock_number":stock_number},{"stock_company":stock_company}], "response": response_message}
+        event=self.dispatchMessage(dispatcher,intent,stock_number,stock_company,response_message)
 
-        # Send response message using dispatcher
-        dispatcher.utter_message(json.dumps(response_dict))
 
-        return [SlotSet("stock_company", stock_company), SlotSet("stock_number", stock_number)]
+        return [AllSlotsReset()]
     
+   
+    
+
 class NavigateAction(Action):
 
     def name(self) -> Text:
@@ -94,24 +82,242 @@ class NavigateAction(Action):
         
         # Get intent and extracted entities
         intent = latest_message['intent']['name']
-        page = None
-        
-        for entity in latest_message['entities']:
-            if entity['entity'] == 'page':
-                page = entity['value']
-        
+        page =tracker.get_slot("page")
         
         # Save extracted entities in slots
         if page:
-            tracker.slots['page'] = page
-        page = tracker.get_slot("page")
-        
-        # Generate response message
-        response_message = f"navigating to {page}"
+            checkPage = CheckPage()
+        # call the run method of CheckPage
+            is_valid=checkPage.run(dispatcher, tracker, domain)["is_valid_page"]
+            if not is_valid:
+                response_message = f"{page} is not present in the application, please specify a valid page"
+            else:
+                response_message = f"navigating to {page}"     
+            # Generate response message
+        else:
+            response_message = f"please specify the name of the page"
         response_dict = {"intent": intent, "entities": {"page":page}, "response": response_message}
-
         # Send response message using dispatcher
         dispatcher.utter_message(json.dumps(response_dict))
 
         return [SlotSet("page", page)]
+        
 
+class FilterAction(Action):
+
+    def name(self) -> Text:
+        return "action_filter"
+    
+    def dispatchMessage(self,dispatcher,intent,filtered_obj,criteria_list,stock_list,response_message):
+         
+        # Create the response dictionary with the retrieved lists
+        response_dict = {"intent": intent, 
+                 "entities": [{"filtered_obj":filtered_obj},
+                              {"criteria":criteria_list},
+                              {"stock_list":stock_list}],
+                 "response": response_message}
+
+        # Send the response back to the user
+        dispatcher.utter_message(json.dumps(response_dict))
+        return []
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        # Get latest user message
+        latest_message = tracker.latest_message
+        
+        # Get intent and extracted entities
+        intent = latest_message['intent']['name']
+        filtered_obj = tracker.get_slot("filtered_obj")
+        criteria_list = tracker.get_slot("criteria")
+        stock_list=tracker.get_slot("stock_list")
+        #check filtered obj
+        if filtered_obj:
+            checkFilteredObj = CheckFilteredObj()
+            # call the run method of CheckPage
+            is_valid=checkFilteredObj.run(dispatcher, tracker, domain)["is_valid_filtered_obj"]
+            if not is_valid:
+                response_message="Please enter a valid object to filter"
+                self.dispatchMessage(dispatcher,intent,filtered_obj,criteria_list,stock_list,response_message)
+                return []
+            tracker.slots['filtered_obj'] = filtered_obj
+        #check criteria_list
+        if criteria_list:
+            tracker.slots['criteria'] = criteria_list
+        if not filtered_obj:
+
+            response_message="Please specify what you need to filter"
+            event=self.dispatchMessage(dispatcher,intent,filtered_obj,criteria_list,stock_list,response_message)
+            return []
+        
+        # Generate response message
+        response_message = f"processing command..."
+        event=self.dispatchMessage(dispatcher,intent,filtered_obj,criteria_list,stock_list,response_message)
+
+
+        return [AllSlotsReset()]
+
+class SearchAction(Action):
+
+    def name(self) -> Text:
+        return "action_search"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        # Get latest user message
+        latest_message = tracker.latest_message
+        
+        # Get intent and extracted entities
+        intent = latest_message['intent']['name']
+        query =tracker.get_slot("query")
+        #if term exists
+        if query:
+            tracker.slots['query'] = query
+            response_message = f"searching for {query}"     
+        else:
+            response_message = f"please specify the search term"
+        response_dict = {"intent": intent, "entities": {"query":query}, "response": response_message}
+        # Send response message using dispatcher
+        dispatcher.utter_message(json.dumps(response_dict))
+
+        return [SlotSet("query", query)]
+        
+
+
+class DefineAction(Action):
+
+    def name(self) -> Text:
+        return "action_define"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        # Get latest user message
+        latest_message = tracker.latest_message
+        
+        # Get intent and extracted entities
+        intent = latest_message['intent']['name']
+        term =tracker.get_slot("term")
+        #if term exists
+        if term:
+            tracker.slots['term'] = term
+            response_message = f"searching for {term}"     
+        else:
+            response_message = f"please specify the term to define"
+        response_dict = {"intent": intent, "entities": {"term":term}, "response": response_message}
+        # Send response message using dispatcher
+        dispatcher.utter_message(json.dumps(response_dict))
+
+        return [AllSlotsReset()]
+    
+
+class SellStockAction(Action):
+
+    def name(self) -> Text:
+        return "action_sell_stock"
+    
+    def dispatchMessage(self,dispatcher,intent,stock_number,stock_company,response_message):
+         
+        response_dict = {"intent": intent, "entities": [{"stock_number":stock_number},{"stock_company":stock_company}], "response": response_message}
+
+        dispatcher.utter_message(json.dumps(response_dict))
+        return []
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        # Get latest user message
+        latest_message = tracker.latest_message
+        
+        # Get intent and extracted entities
+        intent = latest_message['intent']['name']
+        stock_number = tracker.get_slot("stock_number")
+        stock_company = tracker.get_slot("stock_company")
+        
+        if stock_company:
+            event = ActionExecuted("action_check_security")
+            if not event:
+                response_message="Please enter a valid security name"
+                return[]
+            tracker.slots['stock_company'] = stock_company
+        if stock_number:
+            tracker.slots['stock_number'] = stock_number
+
+        if not stock_company and not stock_number:
+
+            response_message="Please provide the stocks number and the name of the security"
+            event=self.dispatchMessage(dispatcher,intent,stock_number,stock_company,response_message)
+            return []
+
+        elif not stock_company:
+            response_message="Please provide the name of the security"
+            event=self.dispatchMessage(dispatcher,intent,stock_number,stock_company,response_message)
+            return [SlotSet("stock_company", stock_company), SlotSet("stock_number", stock_number)]
+
+        elif not stock_number:
+
+            response_message="Please provide the number of stocks"
+            event=self.dispatchMessage(dispatcher,intent,stock_number,stock_company,response_message)
+            return [SlotSet("stock_company", stock_company), SlotSet("stock_number", stock_number)]
+        
+        # Generate response message
+        response_message = f"processing command..."
+        event=self.dispatchMessage(dispatcher,intent,stock_number,stock_company,response_message)
+
+
+        return [AllSlotsReset()]
+
+class GreetAction(Action):
+
+    def name(self) -> Text:
+        return "action_greet"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        # Get latest user message
+        latest_message = tracker.latest_message
+        
+        # Get intent and extracted entities
+        intent = latest_message['intent']['name']
+
+        response = random.choice(domain["responses"]["utter_greet"])
+        response_dict = {"intent": intent, "response":response["text"]}
+        # Send response message using dispatcher
+        dispatcher.utter_message(json.dumps(response_dict))
+
+        return []
+
+class OtherAction(Action):
+
+    def name(self) -> Text:
+        return "action_other"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        # Get latest user message
+        latest_message = tracker.latest_message
+        intent_template_map = {
+            "greet": "utter_greet",
+            "goodbye": "utter_goodbye",
+        }
+        # Get intent and extracted entities
+        intent = latest_message['intent']['name']
+        template_key = intent_template_map.get(intent, "utter_default")
+
+
+        response = random.choice(domain["responses"][template_key])
+        response_dict = {"intent": intent, "response":response["text"]}
+        # Send response message using dispatcher
+        dispatcher.utter_message(json.dumps(response_dict))
+
+        return []
